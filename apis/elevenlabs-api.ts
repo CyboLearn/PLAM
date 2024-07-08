c824447d54be9b731e670a32102ecfd755b22d1d
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-07-08 20:31:11 +0100

    Work on elevenlabs api

---
+++ apis/elevenlabs-api.ts
@@ -0,0 +1,64 @@
+"use server";
+
+type VoiceId = string; // NOSONAR
+
+export type ElevenLabsApiEndpoints =
+	// Text-to-Speech
+	| `text-to-speech/${VoiceId}`
+	| `text-to-speech/${VoiceId}/with-timestamps`
+	| `text-to-speech/${VoiceId}/stream`
+	| `text-to-speech/${VoiceId}/stream/with-timestamps`
+	// Speech-to-Speech
+	| `speech-to-speech/${VoiceId}`
+	| `speech-to-speech/${VoiceId}/stream`
+	// Sound Generation
+	| `sound-generation`
+	// Audio Isolation
+	| `audio-isolation`
+	| `audio-isolation/stream`
+	// History
+	| `history`
+	| `history/download`
+	| `history/${string}`
+	| `history/${string}/audio`
+	// Samples
+	| `voices/${VoiceId}/samples/${string}`
+	| `voices/${VoiceId}/samples/${string}/audio`;
+
+export async function makeElevenLabsApiRequest({
+	endpoint,
+  method = "GET",
+  body
+}: {
+	readonly endpoint: string;
+  readonly method: "GET" | "POST" | "DELETE";
+  readonly body?: any;
+}) {
+	const url = `https://api.elevenlabs.io/v1/${endpoint}`;
+
+	// Make the request
+	const response = await fetch(url, {
+    method,
+    headers: {
+			"xi-api-key": `${process.env.ELEVENLABS_API_KEY}`,
+      "Content-Type": "application/json",
+		},
+    body: JSON.stringify(body)
+  });
+
+	// Check for errors
+	if (!response.ok) {
+		return {
+			data: null,
+			error: {
+				status: response.status,
+				statusText: response.statusText,
+			},
+		};
+	}
+
+	return {
+		data: await response.json(),
+		error: null,
+	};
+}
