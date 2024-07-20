21d12ed1ab8d61eaeb4967761ccdde76d8c3bfd2
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-07-20 12:30:12 +0100

    update 20240720103600_storage_functions.sql

--- supabase/migrations/20240720103600_storage_functions.sql
+++ supabase/migrations/20240720103600_storage_functions.sql
@@ -1,5 +1,5 @@
-DELETE FROM "storage"."buckets" 
-WHERE "id" IN ('media', 'open');
+DELETE FROM "storage"."buckets"
+WHERE "id" = 'media';
 
 INSERT INTO "storage"."buckets" ("id", "name", "owner", "created_at", "updated_at", "public", "avif_autodetection", "file_size_limit", "allowed_mime_types", "owner_id") VALUES
 	('media', 'media', NULL, '2024-07-18 13:42:03.369549+00', '2024-07-18 13:42:03.369549+00', false, false, NULL, '{image/*,audio/*,video/*,application/octet-stream,text/plain}', NULL),
